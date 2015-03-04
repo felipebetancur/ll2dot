e5c3de7a8c674c4d54cc89deab00c51a7afdc01b
mewmew <rnd0x00@gmail.com>
2015-03-04 17:44:23 +0100

    ll2go: Add newline in comment.

--- ll2dot.go
+++ ll2dot.go
@@ -16,6 +16,7 @@
 //    ll2dot [OPTION]... FILE...
 //
 // Flags:
+//
 //    -f=false:   Force overwrite existing graph directories.
 //    -funcs="":  Comma separated list of functions to parse (e.g. "foo,bar").
 //    -img=false: Generate an image representation of the CFG.
