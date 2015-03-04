d0d61aaef838e534d60852c6011ea2ed35238796
mewmew <rnd0x00@gmail.com>
2015-03-04 17:40:30 +0100

    ll2go: Add usage to documentation.

--- ll2dot.go
+++ ll2dot.go
@@ -10,6 +10,16 @@
 //
 //    * foo_graphs/bar.dot
 //    * foo_graphs/baz.dot
+//
+// Usage:
+//
+//    ll2dot [OPTION]... FILE...
+//
+// Flags:
+//    -f=false:   Force overwrite existing graph directories.
+//    -funcs="":  Comma separated list of functions to parse (e.g. "foo,bar").
+//    -img=false: Generate an image representation of the CFG.
+//    -q=false:   Suppress non-error messages.
 package main
 
 import (
